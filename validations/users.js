38dc221a01dbd6b44985ca5030891834de6ed458
SeverineB <severine.bianchi78@gmail.com>
2021-10-30 14:37:58 +0200

    :bug: Edit regex for password register

--- validations/users.js
+++ validations/users.js
@@ -1,9 +1,11 @@
 const Joi = require('joi');
 
+const pattern = "/(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[$@$!#.])[A-Za-zd$@$!%*?&.]{8,20}/";
+
 const authSchema = Joi.object({
   email: Joi.string().email().lowercase().required(),
   username: Joi.string().alphanum().min(3).max(30).required(),
-  password: Joi.string().min(5).pattern(new RegExp('^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$'))
+  password: Joi.string().min(5).pattern(new RegExp(pattern))
 })
 
 module.exports = {
