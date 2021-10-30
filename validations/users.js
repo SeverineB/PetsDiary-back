6e165b964494ebac8cfd2074a357267985ac38fa
SeverineB <severine.bianchi78@gmail.com>
2021-10-30 14:21:12 +0200

    :bug: Change password for register

--- validations/users.js
+++ validations/users.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 const authSchema = Joi.object({
   email: Joi.string().email().lowercase().required(),
   username: Joi.string().alphanum().min(3).max(30).required(),
-  password: Joi.string().min(5).pattern(new RegExp('^[a-zA-Z0-9][!@#$%¨&*]{5,30}$'))
+  password: Joi.string().min(5).pattern(new RegExp('^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$'))
 })
 
 module.exports = {
